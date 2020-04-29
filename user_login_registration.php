233f1e57807afc21cb94f1627e7fac8b3098800a
roguerre <iv.gubri@yahoo.com>
2020-04-29 15:45:04 -0500

    Incluir jquery en login y registro de usuarios

--- user_login_registration.php
+++ user_login_registration.php
@@ -31,6 +31,9 @@
             </form>
         </section>
     </section>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.0/jquery.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous">
+    </script>
+    <script src="./files/scripts/user_login_registration.js"></script>
 </body>
 
 </html>
