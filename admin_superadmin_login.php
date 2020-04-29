45fee49b570fce9c0df9c04fd238270303d2c0d9
roguerre <iv.gubri@yahoo.com>
2020-04-29 18:25:23 -0500

    Incluir documento html para login administrativo

---
+++ admin_superadmin_login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Music Store - Acceso</title>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap" rel="stylesheet">
+    <link href="./files/styles/admin_superadmin_login.css" rel="stylesheet">
+</head>
+
+<body>
+    <section class="login-container">
+        <header class="company">
+            <h1>Music Store</h1>
+            <p>Acceso administrativo</p>
+        </header>
+        <main class="login">
+            <form action="">
+                <input type="email" name="correo" placeholder="Email" required>
+                <input type="password" name="contrasena" placeholder="Contraseña" required>
+                <button>Ingresar</button>
+            </form>
+        </main>
+        <footer class="notice">
+            <p>
+                Absténgase de ingresar si usted no es un usuario
+                administrativo.
+                <span>Se aplicarán las acciones legales a que de lugar.</span>
+            </p>
+        </footer>
+    </section>
+</body>
+
+</html>
