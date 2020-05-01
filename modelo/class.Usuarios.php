439d95be5567bb1f54e793f2dd1457e0f6523a0a
roguerre <iv.gubri@yahoo.com>
2020-04-30 20:34:46 -0500

    Incluir rutina de registro en modelo de usuarios
    Esta rutina es generica y se puede reutilizar para registrar
    administradores tambien

--- modelo/class.Usuarios.php
+++ modelo/class.Usuarios.php
@@ -2,12 +2,45 @@
 
 require(__DIR__ . '/../config/class.Conexion.php');
 
-class Usuarios{
+class Usuarios
+{
 
-    public function __construct(){
-
+    public function __construct()
+    {
     }
 
-}
 
+    // Registrar un usuario (solo si su correo no existe)
+    public function userRegistration($nombre, $apellido, $correo, $contrasena, $user_type)
+    {
+        $conexion = new Conexion();
+        $query_non_existent = "SELECT * FROM usuarios WHERE correo = '$correo'";
+        $res = $conexion->query($query_non_existent);
+
+        if ($res->num_rows > 0) {
+            return false;
+        }
+
+        if ($user_type == "user") {
+            $idEstado = 1;
+            $idRol = 1;
+            $query_insert_user = ("INSERT INTO usuarios (nombre, apellido, correo, contrasena, idEstado, idRol) " .
+                "VALUES ('$nombre', '$apellido', '$correo', '$contrasena', $idEstado, $idRol)");
+            $conexion->query($query_insert_user);
+            return true;
+        }
+
+        if ($user_type == "admin") {
+            $idEstado = 1;
+            $idRol = 2;
+            $query_insert_user = ("INSERT INTO usuarios (nombre, apellido, correo, contrasena, idEstado, idRol) " .
+                "VALUES ('$nombre', '$apellido', '$correo', '$contrasena', $idEstado, $idRol)");
+            $conexion->query($query_insert_user);
+            return true;
+        }
+
+        // No es un tipo de usuario valido
+        return false;
+    }
+}
 ?>
