778e081c028485f27b6b287ab75e6b047a45a7a2
juavauj <juavauj@gmail.com>
2020-05-01 07:47:19 -0500

    Modifiqué la clase generos

--- modelo/class.Generos.php
+++ modelo/class.Generos.php
@@ -8,6 +8,17 @@ class Generos{
 
     }
 
+    // Mostrar albumes por genero
+
+    public function mostrarAlbumes(){
+        $db = new Conexion();
+
+        $sql = "SELECT nombre, imagen FROM albumes AS a INNER JOIN generos AS g ON a.idGenero = g.idGenero"
+    }
+
+
+    // Filtrar albumes por genero
+
 }
 
 ?>
