f093801924abf362975892fc8f11e83ff6924a0e
roguerre <iv.gubri@yahoo.com>
2020-05-02 14:15:43 -0500

    Modificar ruta al script de carga dinamica

--- files/subpages/footer.php
+++ files/subpages/footer.php
@@ -88,7 +88,7 @@ menuRes.addEventListener("click", function dropDown() {
     }
 })
     </script>
-    <script src="./scripts/cargaDinamicaAlbumesYGeneros.js"></script>
+    <script src="files/subpages/scripts/cargaDinamicaAlbumesYGeneros.js"></script>
 </body>
 <link rel="stylesheet" href="files/subpages/styles/estilos.css">
 </html>
